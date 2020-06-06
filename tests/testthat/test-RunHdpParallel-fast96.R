8c6fe63eed0e310f851dc645cf87e5e1d6eefce4
jnh01 <jiangnanhai@hotmail.com>
2020-06-06 09:56:24 +0800

    Updated the regression test data.

--- tests/testthat/test-RunHdpParallel-fast96.R
+++ tests/testthat/test-RunHdpParallel-fast96.R
@@ -22,7 +22,7 @@ test_that("RunHdpParallel-fast96", {
     overwrite     = TRUE
   )
 
-  #save(retvalx, file = "RunhdpInternal.testdata/RunHdpParallel-fast96-2-cores.Rdata")
+  # save(retvalx, file = "RunhdpInternal.testdata/RunHdpParallel-fast96-2-cores.Rdata")
   expect_equal(retvalx$signature, regression$retvalx$signature)
   expect_equal(retvalx$exposure, regression$retvalx$exposure)
   expect_equal(retvalx$multi.chains, regression$retvalx$multi.chains)
