a83d5eb33cd491b4de456314ec3032de8355c09d
Steven G. Rozen <steve@steverozen.net>
2020-06-09 14:04:44 +0800

    Code clean up and correct test

--- R/SetupAndPosterior.R
+++ R/SetupAndPosterior.R
@@ -1,35 +1,8 @@
 #' Generate an HDP Gibbs sampling chain from a spectra catalog.
 #'
-#' @param input.catalog Input spectra catalog as a matrix or
-#' in \code{\link[ICAMS]{ICAMS}} format.
+#' @inheritParams PrepInit
 #'
-#' @param seedNumber An integer that is used to generate separate
-#'   random seeds for each call to \code{\link[hdpx]{dp_activate}},
-#'   and each call of \code{\link[hdpx]{hdp_posterior}}; please see the code
-#'   on how this is done. But repeated calls with same value of
-#'   \code{seedNumber} and other inputs should produce the same results.
-#'
-#' @param K.guess Initial guess of the number of "raw clusters",
-#' which may be larger than the number of signatures
-#' (sometimes called "components" in the hdpx code);
-#'  passed to \code{\link[hdpx]{dp_activate}} as
-#' \code{initcc}.
-#'
-#' @param multi.types A logical scalar or
-#' a character vector.
-#' If \code{FALSE}, The HDP analysis
-#'   will regard all input spectra as one tumor type.
-#'
-#' If \code{TRUE}, the HDP analysis
-#'   will infer tumor types based on the string before "::" in their names.
-#' e.g. tumor type for "SA.Syn.Ovary-AdenoCA::S.500" would be "SA.Syn.Ovary-AdenoCA"
-#'
-#' If \code{multi.types} is a character vector, then it should be of the same length
-#' as the number of columns in \code{input.catalog}, and each value is the
-#' name of the tumor type of the corresponding column in \code{input.catalog},
-#' e.g. \code{c("SA.Syn.Ovary-AdenoCA", "SA.Syn.Ovary-AdenoCA", "SA.Syn.Kidney-RCC")}.
-#'
-#' @param verbose If \code{TRUE} then \code{message} progress information.
+#' @inheritParams SetupAndActivate
 #'
 #' @param post.burnin Pass to \code{\link[hdpx]{hdp_posterior}}
 #'      \code{burnin}.
@@ -45,9 +18,6 @@
 #'
 #' @param post.verbosity Pass to \code{\link[hdpx]{hdp_posterior}}
 #'      \code{verbosity}.
-#'
-#' @param gamma.alpha shape of gamma distribution
-#' @param gamma.beta inverse scale of gamma distribution
 #'
 #' @return Invisibly, an \code{\link[hdpx]{hdpSampleChain-class}} object
 #'  as returned from \code{\link[hdpx]{hdp_posterior}}.
@@ -69,50 +39,13 @@ SetupAndPosterior <-
            gamma.beta          = 1)
 { # 12 arguments
 
-    # if (!exists("stir.closure", envir = .GlobalEnv)) {
-    #   assign("stir.closure", hdpx::xmake.s(), envir = .GlobalEnv)
-    # }
-
-
-    prep_val <- PrepInit(multi.types = multi.types,
-                         input.catalog = input.catalog,
-                         verbose       = verbose,
-                         K.guess       = K.guess,
-                         gamma.alpha   = gamma.alpha,
-                         gamma.beta    = gamma.beta)
-
-    if (verbose) message("calling hdp_init ", Sys.time())
-    hdpObject <- hdpx::hdp_init(ppindex = prep_val$ppindex,
-                                cpindex = prep_val$cpindex,
-                                hh      = rep(1,prep_val$number.channels),
-                                alphaa  = prep_val$alphaa, ##The prior is a gamma with shape alphaa, and inverse scale alphab
-                                alphab  = prep_val$alphab)
-
-    # num.process is the number of samples plus number of cancer types plus 1 (grandparent)
-    num.process <- hdpx::numdp(hdpObject)
-
-    if (verbose) message("calling hdp_setdata ", Sys.time())
-
-    # (hdp/hdpx)::hdp_setdata generates the warning:
-    # In if (!class(data) %in% c("matrix", "data.frame")) { :
-    #     the condition has length > 1 and only the first element will be used
-    # We circumvent this here
-    tmp.cs <- prep_val$convSpectra
-    attr(tmp.cs, "class") <- "matrix"
-    hdpObject <-
-      hdpx::hdp_setdata(hdpObject,
-                        (1 + prep_val$num.tumor.types + 1):num.process,
-                        tmp.cs)
-    rm(tmp.cs)
-
-    if (verbose) message("calling dp_activate ", Sys.time())
-    # dp_activate requires that stir.closure exists in .GlobalEnv;
-    # see above in this function.
-
-    hdp.state <- hdpx::dp_activate(hdp     = hdpObject,
-                                   dpindex = 1:num.process,
-                                   initcc  = K.guess,
-                                   seed    = seedNumber)
+    hdp.state <- SetupAndActivate(input.catalog = input.catalog,
+                                  seedNumber    = seedNumber,
+                                  K.guess       = K.guess,
+                                  multi.types   = multi.types,
+                                  verbose       = verbose,
+                                  gamma.alpha   = gamma.alpha,
+                                  gamma.beta    = gamma.beta)
 
     if (verbose) message("calling hdp_posterior, seed = ",
                          seedNumber, " ", Sys.time())
