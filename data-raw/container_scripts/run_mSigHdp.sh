2cea1b467c51ef313bf449375dea97b4fe411b47
Steve Rozen <steverozen@gmail.com>
2022-10-07 13:59:39 +0800

    removed system and user specific path

--- data-raw/container_scripts/run_mSigHdp.sh
+++ data-raw/container_scripts/run_mSigHdp.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-PROJECT_DIR="/home/e0012078/container/msighdp"
+PROJECT_DIR="."
 SEED="519"
 K_GUESS="26"
-OUTPUT_DIR="/home/e0012078/output/msighdp"
-
-singularity exec -e $PROJECT_DIR/msighdp.sif nohup Rscript --vanilla $PROJECT_DIR/test_mSigHdp.R $PROJECT_DIR $SEED $K_GUESS $OUTPUT_DIR > $PROJECT_DIR/msighdp.log 2>&1 &
+OUTPUT_DIR="."
+singularity exec -e $PROJECT_DIR/msighdp_2.1.0.3.sif nohup Rscript --vanilla $PROJECT_DIR/test_mSigHdp.R $PROJECT_DIR $SEED $K_GUESS $OUTPUT_DIR > $PROJECT_DIR/msighdp.log 2>&1 &
