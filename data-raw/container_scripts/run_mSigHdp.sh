ed5fbc364c101a8b67cc0b5bcd256e7b5a50a043
Steven G. Rozen <steve@steverozen.net>
2022-10-10 16:26:49 +0800

    simplifying example

--- data-raw/container_scripts/run_mSigHdp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROJECT_DIR="."
-SEED="519"
-K_GUESS="26"
-OUTPUT_DIR="."
-singularity exec -e $PROJECT_DIR/msighdp_2.1.0.3.sif nohup Rscript --vanilla $PROJECT_DIR/test_mSigHdp.R $PROJECT_DIR $SEED $K_GUESS $OUTPUT_DIR > $PROJECT_DIR/msighdp.log 2>&1 &
--- data-raw/container_scripts/test_mSigHdp.R
+++ data-raw/container_scripts/test_mSigHdp.R
@@ -1,48 +1,56 @@
-#!/usr/bin/env Rscript
+# This is designed to run in a singularity/apptainer container.
+# Do not call it directly from the command line.
+#
+# Suggested call for simple example
+# singularity exec <my_R_container> Rscript --vanilla test_mSigHdp.R 123
+#
+# But normally something like:
+# nice singularity exec <my_R_container> Rscript --vanilla test_mSigHdp.R 123 >& log.txt &
 
-use_container <- TRUE
+# We need to tell R to look for packages first inside the container
+# but also to be able to look outside the container (the users "host"
+# system). This works only if the host OS is the same
+# the container OS, or the libraries in the host system do not
+# have compiled code. In the last case, you would have to
+# convert the container to sandbox, add the necessary libraries,
+# and then convert back to a .sif.
+.libPaths(c("/usr/local/lib/R/site-library", .libPaths()))
 
-if (use_container) {
-  # If using singularity container to run this script, need to tell R to look
-  # for packages in the path inside the container. Otherwise, R will use the
-  # library path in the host system, which can cause package dependency errors
-  .libPaths("/usr/local/lib/R/site-library")
-
-  # It is recommended to use bash script to pass arguments to this R script
-  # when running mSigHdp using singularity container
-
-  # args have the arguments passed to Rscript from bash script
-  args <- commandArgs(trailingOnly = TRUE)
-  project_dir <- args[1]
-  seed <- as.numeric(args[2])
-  K_guess <- as.numeric(args[3])
-  output_dir <- args[4]
-} else {
-  project_dir <- "/home/e0012078/container/msighdp"
-  seed <- 519
-  K_guess <- 26
-  output_dir <- "/home/e0012078/output/msighdp"
+# args have the arguments passed to Rscript from bash script
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  stop("Please provide the random seed as an argument")
 }
+seed <- args[1]
 
 library(ICAMS)
 library(mSigHdp)
 
-input_file <- file.path(project_dir, "test_catalog.csv")
+# All inputs and outputs are in the current working directory
+
+input_file <- "test_catalog.csv"
 input_catalog <- ICAMS::ReadCatalog(file = input_file)
 
-output_home <- file.path(output_dir, paste0("seed_", seed))
+output_home <- paste0("seed_", seed)
 
 if (!dir.exists(output_home)) {
-  dir.create(path = output_home, recursive = TRUE)
+  if (!dir.create(path = output_home, recursive = TRUE)) {
+    stop("unable to create ", output_home)
+  }
 }
 
 checkpoint_dir <- file.path(output_home, "checkpoint")
 if (!dir.exists(checkpoint_dir)) {
-  dir.create(path = checkpoint_dir, recursive = TRUE)
+  if (!dir.create(path = checkpoint_dir, recursive = TRUE)) {
+    stop("unable to create ", checkpoint_dir)
+  }
 }
+
 # Set working directory to checkpoint_dir directory so that the checkpoint Rdata
 # files will be saved there
 setwd(checkpoint_dir)
+
+K_guess <- 26
 
 message("Start running mSigHdp")
 message("Using seed ", seed)
@@ -50,36 +58,31 @@ message("Using K_guess ", K_guess)
 message("mSigHdp version: ")
 print(packageVersion("mSigHdp"))
 
-time_used <- system.time(expr = {
-  # The parameters used here are for testing purpose only to save time.
-  # For recommended parameters to use, please refer to this paper
-  # Mo Liu, Yang Wu, Nanhai Jiang, Arnoud Boot, Steven G. Rozen,
-  # mSigHdp: hierarchical Dirichlet process mixture modeling
-  # for mutational signature discovery,
-  # https://www.biorxiv.org/content/10.1101/2022.01.31.478587v1
+# The parameters used here are for testing purpose only to save time.
+# For recommended parameters to use, please refer to this paper
+# Mo Liu, Yang Wu, Nanhai Jiang, Arnoud Boot, Steven G. Rozen,
+# mSigHdp: hierarchical Dirichlet process mixture modeling
+# for mutational signature discovery,
+# https://www.biorxiv.org/content/10.1101/2022.01.31.478587v1
 
-  mSigHdp::RunHdpxParallel(
-    input.catalog = input_catalog,
-    seedNumber = seed,
-    K.guess = K_guess,
-    out.dir = output_home,
-    multi.types = FALSE,
-    burnin = 1000,
-    burnin.multiplier = 2,
-    post.n = 5,
-    post.space = 10,
-    num.child.process = 2,
-    CPU.cores = 2,
-    high.confidence.prop = 0.9,
-    gamma.alpha = 1,
-    gamma.beta = 20,
-    checkpoint = TRUE,
-    verbose = FALSE,
-    downsample_threshold = 3000
-  )
-})
+mSigHdp::RunHdpxParallel(
+  input.catalog = input_catalog,
+  seedNumber = seed,
+  K.guess = K_guess,
+  out.dir = output_home,
+  multi.types = FALSE,
+  burnin = 1000,
+  burnin.multiplier = 2,
+  post.n = 5,
+  post.space = 10,
+  num.child.process = 2,
+  CPU.cores = 2,
+  high.confidence.prop = 0.9,
+  gamma.alpha = 1,
+  gamma.beta = 20,
+  checkpoint = TRUE,
+  verbose = FALSE,
+  downsample_threshold = 3000
+)
 
-saveRDS(time_used, file = file.path(output_home, "time_used.Rds"))
-message("Time used: ")
-print(time_used)
 message("Finished running mSigHdp")
