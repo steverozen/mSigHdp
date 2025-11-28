22a0887fa816f35487b93dc4d39dcc427280b223
Steven G. Rozen <steverozen@pm.me>
2025-11-28 11:30:11 -0500

    simplified tests given change from warning to message

--- tests/testthat/test-degenerate_hpdx_extract_components.R
+++ tests/testthat/test-degenerate_hpdx_extract_components.R
@@ -4,7 +4,7 @@ test_that("degenerate_hdpx_extract_components", {
   input.catalog <-
     read.csv("SBS96.ground.truth/ground.truth.syn.catalog.csv")[1:10, c(3, 3)]
 
-  retvalx <- suppressWarnings({
+  retvalx <-
     RunHdpxParallel(
       input.catalog = input.catalog,
       CPU.cores = 2,
@@ -21,33 +21,6 @@ test_that("degenerate_hdpx_extract_components", {
       downsample_threshold = 1e6, # Very high; should have no effect
       out.dir = tempfile()
     )
-  })
 
   expect_snapshot(retvalx)
-})
-
-test_that("degenerate_hdpx_extract_components_warnings", {
-  testthat::local_edition(3)
-
-  input.catalog <-
-    read.csv("SBS96.ground.truth/ground.truth.syn.catalog.csv")[1:10, c(3, 3)]
-
-  expect_snapshot_warning(
-    RunHdpxParallel(
-      input.catalog = input.catalog,
-      CPU.cores = 2,
-      seedNumber = 44,
-      K.guess = 5,
-      multi.types = FALSE,
-      verbose = FALSE,
-      num.child.process = 2,
-      burnin = 50, # Super low for fast testing
-      post.space = 5, # Low for fast testing
-      post.cpiter = 1, # Low for fast testing
-      overwrite = TRUE,
-      checkpoint = FALSE,
-      downsample_threshold = 1e6, # Very high; should have no effect
-      out.dir = tempfile()
-    )
-  )
 })
