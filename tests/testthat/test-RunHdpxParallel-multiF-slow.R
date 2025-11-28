12f6e6fc9a478c486426756bbabc3e452dbfee90
Steven G. Rozen <steverozen@pm.me>
2025-11-28 11:31:11 -0500

    reduce number of child processes for lower memory machines

--- tests/testthat/test-RunHdpxParallel-multiF-slow.R
+++ tests/testthat/test-RunHdpxParallel-multiF-slow.R
@@ -1,39 +1,38 @@
-
 test_that("RunHdpxParallel-slow-multiF", {
   if (Sys.getenv("MSIGHDP_LONG") == "") {
     # Sys.setenv(MSIGHDP_LONG="Y")
-    skip("Sys.setenv(MSIGHDP_LONG=\"Y\") to run test-RunHdpxParallel-multiF-slow")
+    skip(
+      "Sys.setenv(MSIGHDP_LONG=\"Y\") to run test-RunHdpxParallel-multiF-slow"
+    )
   }
   require(ICAMS)
   input.catalog <- PCAWG7::spectra$PCAWG$ID
   input.catalog <-
-    input.catalog[ , colSums(input.catalog) > 50, drop = FALSE]
+    input.catalog[, colSums(input.catalog) > 50, drop = FALSE]
   set.seed(1)
   input.catalog <-
-    input.catalog[ , sort(sample(ncol(input.catalog), 500, replace = FALSE))]
+    input.catalog[, sort(sample(ncol(input.catalog), 500, replace = FALSE))]
 
   reg <- new.env()
   load("tdata/RunHdpParallel-slow-multiF.Rdata", envir = reg)
 
   retvalx <- RunHdpxParallel(
-    input.catalog     = input.catalog,
-    CPU.cores         = 20,
-    seedNumber        = 44,
-    K.guess           = 5,
-    multi.types       = FALSE,
-    verbose           = FALSE,
+    input.catalog = input.catalog,
+    CPU.cores = 10, # in one test each core used 1.7G / my test machine has 31G assuming 20G avail, 20/17 = 11.7
+    seedNumber = 44,
+    K.guess = 5,
+    multi.types = FALSE,
+    verbose = FALSE,
     num.child.process = 20,
-    burnin            = 500,
-    post.space        = 50,
-    post.n            = 25,
-    overwrite         = TRUE,
-    checkpoint        = FALSE,
-    out.dir           = tempfile()
-
+    burnin = 500,
+    post.space = 50,
+    post.n = 25,
+    overwrite = TRUE,
+    checkpoint = FALSE,
+    out.dir = tempfile()
   )
 
   # To save a new version of the output:
   # save(retvalx, file = "tdata/RunHdpParallel-slow-multiF.Rdata")
   expect_equal(retvalx, reg$retvalx)
-
 })
--- tests/testthat/test-RunHdpxParallel-multiT-slow.R
+++ tests/testthat/test-RunHdpxParallel-multiT-slow.R
@@ -1,39 +1,38 @@
-
 test_that("RunHdpxParallel-slow-multiT", {
   if (Sys.getenv("MSIGHDP_LONG") == "") {
-    skip("Sys.setenv(MSIGHDP_LONG=\"Y\") to run test-RunHdpxParallel-multiT-slow")
+    skip(
+      "Sys.setenv(MSIGHDP_LONG=\"Y\") to run test-RunHdpxParallel-multiT-slow"
+    )
     # Sys.setenv(MSIGHDP_LONG = "Y")
   }
   require(ICAMS)
   input.catalog <- PCAWG7::spectra$PCAWG$ID
   input.catalog <-
-    input.catalog[ , colSums(input.catalog) > 50, drop = FALSE]
+    input.catalog[, colSums(input.catalog) > 50, drop = FALSE]
   set.seed(1)
   input.catalog <-
-    input.catalog[ , sort(sample(ncol(input.catalog), 500, replace = FALSE))]
+    input.catalog[, sort(sample(ncol(input.catalog), 500, replace = FALSE))]
 
   reg <- new.env()
   load("tdata/RunHdpParallel-slow-multiT.Rdata", envir = reg)
 
   retvalx <- RunHdpxParallel(
-    input.catalog     = input.catalog,
-    CPU.cores         = 20,
-    seedNumber        = 44,
-    K.guess           = 5,
-    multi.types       = TRUE,
-    verbose           = FALSE,
+    input.catalog = input.catalog,
+    CPU.cores = 10, # in one test each core used 1.7G / my test machine has 31G assuming 20G avail, 20/17 = 11.7
+    seedNumber = 44,
+    K.guess = 5,
+    multi.types = TRUE,
+    verbose = FALSE,
     num.child.process = 20,
-    burnin            = 500,
-    post.space        = 50,
-    post.n            = 25,
-    overwrite         = TRUE,
-    checkpoint        = FALSE,
-    out.dir           = tempfile()
-
+    burnin = 500,
+    post.space = 50,
+    post.n = 25,
+    overwrite = TRUE,
+    checkpoint = FALSE,
+    out.dir = tempfile()
   )
 
   # To save a new version of the output:
   # save(retvalx, file = "tdata/RunHdpParallel-slow-multiT.Rdata")
   expect_equal(retvalx, reg$retvalx)
-
 })
